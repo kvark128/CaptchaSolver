74e5d12d9af75fa8d09cdaed275d0daa4f5f5a30
kvark128 <kvark128@yandex.ru>
2018-07-26 20:53:42 +0800

    Remove error module

--- globalPlugins/captchaSolver/__init__.py
+++ globalPlugins/captchaSolver/__init__.py
@@ -9,6 +9,7 @@ from urllib import urlencode
 import globalPluginHandler
 import wx
 import gui
+from logHandler import log
 import scriptHandler
 import addonHandler
 import ui
@@ -16,10 +17,26 @@ import globalVars
 import api
 import speech
 import controlTypes
-from errors import getErrorDescription
 import _config
 
 addonHandler.initTranslation()
+
+ERRORS = {
+	'OFF_SCREEN': _('Captcha off screen'),
+	'CAPTCHA_HAS_NO_LOCATION': _('Captcha has no location'),
+	'ERROR_CONNECTING_TO_SERVER': _('Error connecting to server. Please check your Internet connection'),
+	'ERROR_WRONG_USER_KEY': _('API key is not specified'),
+	'ERROR_KEY_DOES_NOT_EXIST': _('Used a non-existent API key'),
+	'ERROR_ZERO_BALANCE': _('The balance of your account is zero'),
+	'ERROR_NO_SLOT_AVAILABLE': _('The current recognition rate is higher than the maximum set in the settings of Your account. Either on the server queue builds up and employees do not have time to disassemble it, repeat the sending captcha after 5 seconds'),
+	'ERROR_ZERO_CAPTCHA_FILESIZE': _('Size of the captcha is less than 100 bytes'),
+	'ERROR_TOO_BIG_CAPTCHA_FILESIZE': _('Size of the captcha more than 100 KB'),
+	'ERROR_IP_NOT_ALLOWED': _('In Your account you have configured restrictions based on IP from which you can make requests. And the IP from which the request is not included in the allowed list'),
+	'IP_BANNED': _('IP address from which the request is blocked because of frequent requests with various incorrect API keys. The lock is released in an hour'),
+	'ERROR_CAPTCHA_UNSOLVABLE': _('Captcha could not solve 3 different employee. Money for this image come back to balance'),
+	'ERROR_BAD_DUPLICATES': _('The error appears when 100 percent recognition. Has been used the maximum number of attempts, but the required number of identical answers has not been received'),
+	'ERROR_CAPTCHAIMAGE_BLOCKED': _('This captcha can not be recognized'),
+}
 
 class SettingsDialog(gui.SettingsDialog):
 	title = _('Captcha Solver Settings')
@@ -145,17 +162,24 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, lambda evt: os.startfile('https://github.com/kvark128/captchaSolver'), item)
 		menu_tools.AppendMenu(wx.ID_ANY, _('Captcha Solver'), menu_CaptchaSolver)
 
+	def getErrorDescription(self, error):
+		text = ERRORS.get(error)
+		if not isinstance(text, basestring):
+			text = _('Error: {}').format(error)
+		log.error(error)
+		return text
+
 	def balanceDialog(self, status, request):
 		if status:
 			text = _('{:.2f} rubles').format(float(request))
 		else:
-			text = getErrorDescription(request)
+			text = self.getErrorDescription(request)
 
 		gui.messageBox(text, _('Your account balance'))
 
 	def captchaHandler(self, status, request):
 		if not status:
-			ui.message(getErrorDescription(request))
+			ui.message(self.getErrorDescription(request))
 			return
 
 		api.copyToClip(request)
@@ -163,7 +187,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def balanceHandler(self, status, request):
 		if not status:
-			ui.message(getErrorDescription(request))
+			ui.message(self.getErrorDescription(request))
 			return
 
 		ui.message(_('Balance: {:.2f}').format(float(request)))
@@ -187,13 +211,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 		obj = api.getNavigatorObject()
 		if controlTypes.STATE_OFFSCREEN in obj.states:
-			ui.message(getErrorDescription('OFF_SCREEN'))
+			ui.message(self.getErrorDescription('OFF_SCREEN'))
 			return
 
 		try:
 			x, y, width, height = obj.location
 		except:
-			ui.message(getErrorDescription('CAPTCHA_HAS_NO_LOCATION'))
+			ui.message(self.getErrorDescription('CAPTCHA_HAS_NO_LOCATION'))
 			return
 
 		if _config.conf['sizeReport'] and scriptHandler.getLastScriptRepeatCount() != 1:
