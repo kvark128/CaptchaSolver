42ae920af6b7bbf1e267fbe310199c5cdf77cd3b
kvark128 <kvark128@yandex.ru>
2019-03-11 15:37:35 +0800

    Removed support for WXPython 3.0. The minimum required version of NVDA is now 2019.1

--- installTasks.py
+++ installTasks.py
@@ -4,8 +4,9 @@ import gui
 import wx
 
 addonHandler.initTranslation()
+MIN_VERSION = (2019, 1)
 
 def onInstall():
-	if (version_year, version_major) < (2017, 3):
-		gui.messageBox(_('CaptchaSolver incompatible with your NVDA version. CaptchaSolver requires NVDA 2017.3 or later.'), _('Incompatible NVDA version'), style=wx.OK | wx.ICON_ERROR)
-		raise RuntimeError('Incompatible NVDA version')
+	if (version_year, version_major) < MIN_VERSION:
+		gui.messageBox(_("CaptchaSolver incompatible with your NVDA version. CaptchaSolver requires NVDA {version} or later.").format(version=".".join(map(lambda n: str(n), MIN_VERSION))), _("Incompatible NVDA version"), style=wx.OK | wx.ICON_ERROR)
+		raise RuntimeError("Incompatible NVDA version")
